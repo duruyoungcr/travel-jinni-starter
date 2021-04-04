b351d1d7178a6aa32e641a0a88af9a7a475c3f76
Awodein Iyanu <81914687+Iyanuoluwa-creator@users.noreply.github.com>
2021-04-04 22:27:47 +0100

    Add files via upload

--- travel-jinni-starter/pages/index.js
+++ travel-jinni-starter/pages/index.js
@@ -5,23 +5,24 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Reproduce App</title>
+                <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="bg-gray-300">
+
+            <div className="bg-gray-50">
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                     <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                        <span className="block">Ready to Start Learning?</span>
-                        <span className="block text-blue-600">Travel Jinni Nextjs </span>
-                        <span className="block text-black-600">And</span>
-                        <span className="block text-blue-600">Tailwind Starter</span>
+                        <span className="block">Ready to dive in?</span>
+                        <span className="block text-indigo-600">
+                            Travel Jinni Nextjs and Tailwind Starter
+                        </span>
                     </h2>
                     <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                         <div className="inline-flex rounded-md shadow">
                             <a
                                 href="wwww.traveljinni.com"
                                 className="inline-flex items-center justify-center px-5 py-3  ml-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-                                Enroll
+                                Get started
                             </a>
                         </div>
                         <div className="ml-3 inline-flex rounded-md shadow">
